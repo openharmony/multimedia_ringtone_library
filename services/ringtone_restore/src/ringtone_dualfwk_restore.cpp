008b2ef925901359faa76643cf6419659d5984d5
janneyli <liyang782@h-partners.com>
2026-09-05 07:28:38 +0800

    update: 更新文件 ringtone_dualfwk_restore.cpp
    Signed-off-by: janneyli <liyang782@h-partners.com>

--- services/ringtone_restore/src/ringtone_dualfwk_restore.cpp
+++ services/ringtone_restore/src/ringtone_dualfwk_restore.cpp
@@ -515,7 +515,7 @@ CardScenarioType RingtoneDualFwkRestore::AnalyzeCardScenario(bool hasSim1, bool 
         if (hasESim2) return SCENARIO_ESIM2_ONLY;
         if (hasSim1) return SCENARIO_SIM1_ONLY;
         if (hasSim2) return SCENARIO_SIM2_ONLY;
-    } else if (cardCount == 2) { // 2 SIM_LABLE_INDEX_TOW
+    } else if (cardCount == 2) {
         if (hasSim1 && hasSim2) return SCENARIO_SIM1_SIM2;
         if (hasSim1 && hasESim1) return SCENARIO_SIM1_ESIM1;
         if (hasSim1 && hasESim2) return SCENARIO_SIM1_ESIM2;
